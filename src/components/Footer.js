73f64d9c4af650bcda300375d327568960702129
Sergii Bomko <aquila.ua@gmail.com>
2021-07-12 11:34:37 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -11,6 +12,9 @@ const useStyles = makeStyles(() => ({
     alignItems: 'center',
     background: 'transparent',
     boxShadow: 'none'
+  },
+  info: {
+    marginTop: 20
   }
 }));
 
@@ -19,10 +23,16 @@ export default function Footer() {
 
   return (
     <AppBar position='static' className={classes.footer}>
+      <Typography color='textSecondary' variant='subtitle2' className={classes.info}>
+        This is open source project. It uses Ethereum blockchain as a datasource. It does not collect any user's data nor analytics.
+      </Typography>
+      <Typography color='textSecondary' variant='subtitle2'>
+        The best ways to leave a feedback are star the project or create an issue on <Link href='//github.com/aquiladev/web3-domain-manager' target='_blank' rel='noopener'>GitHub</Link>.
+      </Typography>
       <Toolbar>
         <Link href='//github.com/aquiladev/web3-domain-manager' target='_blank' rel='noopener'>
           <IconButton color='default' aria-label='GitHub repo' component='span'>
-            <GitHubIcon fontSize='large' />
+            <GitHubIcon fontSize='default' />
           </IconButton>
         </Link>
       </Toolbar>
