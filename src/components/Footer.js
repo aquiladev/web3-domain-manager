d7d82ae6b6e3ffd9cf3df7e59cd4eb5d2874c2fa
Sergii Bomko <1164492+aquiladev@users.noreply.github.com>
2022-12-17 17:19:04 +0200

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import IconButton from '@material-ui/core/IconButton';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import Chip from '@material-ui/core/Chip';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Chip from "@material-ui/core/Chip";
+import GitHubButton from "react-github-btn";
 
 const useStyles = makeStyles(() => ({
   footer: {
@@ -31,14 +30,41 @@ export default function Footer() {
         The best way to leave a feedback are star the project or create an issue on <Link href='//github.com/aquiladev/web3-domain-manager' target='_blank' rel='noopener'>GitHub</Link>.
       </Typography>
       <Toolbar>
-        <Link href='//github.com/aquiladev/web3-domain-manager' target='_blank' rel='noopener'>
-          <IconButton color='default' aria-label='GitHub repo' component='span'>
-            <GitHubIcon fontSize='default' />
-          </IconButton>
-          {process.env.REACT_APP_GITHUB_REF_SHA &&
-            <Chip label={process.env.REACT_APP_GITHUB_REF_SHA} variant='outlined' />
-          }
-        </Link>
+        <span style={{ marginRight: 12 }}>
+          <GitHubButton
+            href="https://github.com/aquiladev/web3-domain-manager"
+            data-icon="octicon-star"
+            data-size="large"
+            aria-label="Star aquiladev/web3-domain-manager on GitHub"
+          >
+            Star
+          </GitHubButton>
+        </span>
+        <span style={{ marginRight: 12 }}>
+          <GitHubButton
+            href="https://github.com/aquiladev/web3-domain-manager/issues"
+            data-icon="octicon-issue-opened"
+            data-size="large"
+            aria-label="Issue aquiladev/web3-domain-manager on GitHub"
+          >
+            Issue
+          </GitHubButton>
+        </span>
+        <span style={{ marginRight: 12 }}>
+          <GitHubButton
+            href="https://github.com/aquiladev"
+            data-size="large"
+            aria-label="Follow @aquiladev on GitHub"
+          >
+            Follow @aquiladev
+          </GitHubButton>
+        </span>
+        {process.env.REACT_APP_GITHUB_REF_SHA && (
+          <Chip
+            label={process.env.REACT_APP_GITHUB_REF_SHA}
+            variant="outlined"
+          />
+        )}
       </Toolbar>
     </AppBar>
   );
